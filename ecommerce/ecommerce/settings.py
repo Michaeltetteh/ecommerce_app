d6c1529e9a47c19f60e5de9e73f356e8ae5e2495
mike Tetteh <miketetteh751@outlook.com>
2019-08-01 22:55:08 +0000

    contact us form test complete

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,17 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
+
+# Email config
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = "username"
+    EMAIL_HOST = 'smtp.domain.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = "password"
+else:
+    EMAIL_BACKEND = ("django.core.mail.backends.console.EmailBackend")
 
 # Logging
 LOGGING = {
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include(main.urls),namespace='main'),
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', include('main.urls', namespace='main')),
+    # path('', TemplateView.as_view(template_name='home.html')),
 ]
