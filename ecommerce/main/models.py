fe412f9d6a310b4e9f80308f8570b3ca131089de
mike Tetteh <miketetteh751@outlook.com>
2019-08-02 13:36:54 +0000

    added thumbnail field to productimage model

--- ecommerce/main/models.py
+++ ecommerce/main/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="product-images")
+    thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
 
 
 class ProductTag(models.Model):
