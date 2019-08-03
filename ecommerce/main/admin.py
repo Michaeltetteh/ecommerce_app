751829490e012993ee320da3110596d2ae5ff4f9
mike Tetteh <miketetteh751@outlook.com>
2019-08-03 22:14:42 +0000

    Capitized Product name using data migration

--- ecommerce/main/admin.py
+++ ecommerce/main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, ProductImage, ProductTag
+from
+# from .models import Product, ProductImage, ProductTag
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(ProductImage)
-admin.site.register(ProductTag)
+# # Register your models here.
+# admin.site.register(Product)
+# admin.site.register(ProductImage)
+# admin.site.register(ProductTag)
