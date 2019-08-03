1357223481b7e55097dd7f413f30700a1d53670f
mike Tetteh <miketetteh751@outlook.com>
2019-08-03 23:01:24 +0000

    thumbnail generation not working...

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 # from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     # path('', TemplateView.as_view(template_name='home.html')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
